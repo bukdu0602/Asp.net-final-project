e96248ba2a1c63f21cae29d6ce20d66f0561fe63
Ryan Lim <ryan.hyun@gmail.com>
2021-12-04 15:50:16 -0800

    add scaffold

---
+++ ASP.NET Final Assignment/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using ASP.NET_Final_Assignment.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(ASP.NET_Final_Assignment.Areas.Identity.IdentityHostingStartup))]
+namespace ASP.NET_Final_Assignment.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
--- ASP.NET Final Assignment/obj/Debug/net5.0/Razor/Areas/Identity/Pages/_ViewStart.cshtml.g.cs
+++ ASP.NET Final Assignment/obj/Debug/net5.0/Razor/Areas/Identity/Pages/_ViewStart.cshtml.g.cs
@@ -12,7 +12,29 @@ namespace AspNetCore
     using Microsoft.AspNetCore.Mvc;
     using Microsoft.AspNetCore.Mvc.Rendering;
     using Microsoft.AspNetCore.Mvc.ViewFeatures;
+#nullable restore
+#line 1 "C:\Users\lnh06\OneDrive - nluva.com\BCIT SSD Program\Visual studio projects\ASP.NET Final Assignment\ASP.NET Final Assignment\Areas\Identity\Pages\_ViewImports.cshtml"
+using Microsoft.AspNetCore.Identity;
+
+#line default
+#line hidden
+#nullable disable
+#nullable restore
+#line 2 "C:\Users\lnh06\OneDrive - nluva.com\BCIT SSD Program\Visual studio projects\ASP.NET Final Assignment\ASP.NET Final Assignment\Areas\Identity\Pages\_ViewImports.cshtml"
+using ASP.NET_Final_Assignment.Areas.Identity;
+
+#line default
+#line hidden
+#nullable disable
+#nullable restore
+#line 3 "C:\Users\lnh06\OneDrive - nluva.com\BCIT SSD Program\Visual studio projects\ASP.NET Final Assignment\ASP.NET Final Assignment\Areas\Identity\Pages\_ViewImports.cshtml"
+using ASP.NET_Final_Assignment.Areas.Identity.Pages;
+
+#line default
+#line hidden
+#nullable disable
     [global::Microsoft.AspNetCore.Razor.Hosting.RazorSourceChecksumAttribute(@"SHA1", @"13b82ef97968a56a4de533f67cf3e21d662f41aa", @"/Areas/Identity/Pages/_ViewStart.cshtml")]
+    [global::Microsoft.AspNetCore.Razor.Hosting.RazorSourceChecksumAttribute(@"SHA1", @"82ba44b402305ca167ab862266ead2b27f9e9f7d", @"/Areas/Identity/Pages/_ViewImports.cshtml")]
     public class Areas_Identity_Pages__ViewStart : global::Microsoft.AspNetCore.Mvc.Razor.RazorPage<dynamic>
     {
         #pragma warning disable 1998
